5bf105e750edbd285790108fc7173c86ff46b30f
hanfangyu <hanfangyu12345@163.com>
2015-12-23 15:43:20 +0800

    edit GNode setget

--- src/com/data/model/GNode.java
+++ src/com/data/model/GNode.java
@@ -3,5 +3,17 @@ package com.data.model;
 public class GNode
 {
 	private String id;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	private String name;
 }

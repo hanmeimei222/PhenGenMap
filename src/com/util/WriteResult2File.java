9ea19d1718ddc60f56f0a2349e685aab6ef2034f
citoubest <1206539220@qq.com>
2016-01-22 16:55:52 +0800

    完成下载数据的函数

--- src/com/util/WriteResult2File.java
+++ src/com/util/WriteResult2File.java
@@ -4,11 +4,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import com.global.GlobalData;
+import com.model.cytoscape.CytoEdge;
 import com.model.cytoscape.Graph;
 import com.model.d3.D3Graph;
+import com.model.d3.Links;
+import com.model.d3.Node;
 import com.model.d3.TreeNode;
 
 public class WriteResult2File {
@@ -22,8 +27,16 @@ public class WriteResult2File {
 		try
 		{
 			out= new BufferedWriter(new FileWriter(new File(filePath)));
-			//TODO:将查询结果写到文件中
-			out.write(g.toString());
+			Set<CytoEdge> list =g.getEdges();
+			String title = "Atype\tA\tB\n";
+			out.write(title);
+			for (CytoEdge links : list) {
+				String source = links.getData().getSource();
+				String target = links.getData().getTarget();
+				String type = links.getData().getEdgeType();
+				
+				out.write(type+"\t"+source+"\t"+target+"\n");
+			}
 		}
 		catch(IOException e)
 		{
@@ -47,8 +60,19 @@ public class WriteResult2File {
 		try
 		{
 			out= new BufferedWriter(new FileWriter(new File(filePath)));
-			//TODO:将查询结果写到文件中
-			out.write(g.toString());
+			List<Node> nodes = g.getNodes(); 
+			List<Links> list =g.getLinks();
+			String title = "Atype\tAgroup\tAid\tAname\tBtype\tBgroup\tBid\tBname\n";
+			out.write(title);
+			for (Links links : list) {
+				int source = links.getSource();
+				int target = links.getTarget();
+				Node start = nodes.get(source);
+				Node end = nodes.get(target);
+				String startNode = start.getType()+"\t"+ start.getGroup()+"\t"+start.getId()+"\t"+start.getName();
+				String endNode = end.getType()+"\t"+ end.getGroup()+"\t"+end.getId()+"\t"+end.getName();
+				out.write(startNode+"\t"+endNode+"\n");
+			}
 		}
 		catch(IOException e)
 		{
@@ -68,7 +92,25 @@ public class WriteResult2File {
 		String fileName = generateFileName();
 		String filePath = path+fileName;
 
-		return filePath;
+		BufferedWriter out=null;
+		try
+		{
+			out= new BufferedWriter(new FileWriter(new File(filePath)));
+			out.write(tree.toString());
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try {
+				out.close();
+			} catch (IOException e) {
+			}
+		}
+		
+		return fileName;
 	}
 
 	private static String generateFileName()
